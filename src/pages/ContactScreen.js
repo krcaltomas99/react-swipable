7ff14466ed1020e479ac32ecb0f8ec72544cad0f
Tomáš Krčál <krcaltomas99@gmail.com>
2019-09-09 18:30:51 +0200

    Finalizing styles

--- src/pages/ContactScreen.js
+++ src/pages/ContactScreen.js
@@ -5,17 +5,19 @@ import Darker from "../components/Darker";
 class ContactScreen extends Component {
   render() {
     return (
-      <div className='page cn'>
+      <div className="page cn">
         <Darker />
-        <h1>Kontakt</h1>
-        <p>
-          mnou, ať rovněž známost severu uzavřeli. Rysů obrátit ty invazivními
-          přikládání, za je zvyšují celém mě jednu tři u tam co nebo obilí,
-          odstřihne server, a ve patří stát až nádech pánve pro každou
-          rekonstrukce. Nemů
-        </p>
-        <Link to="/">Domů</Link>
-        <Link to="/about">About</Link>
+        <div className="wrap">
+          <h1>Kontakt</h1>
+          <p>
+            mnou, ať rovněž známost severu uzavřeli. Rysů obrátit ty invazivními
+            přikládání, za je zvyšují celém mě jednu tři u tam co nebo obilí,
+            odstřihne server, a ve patří stát až nádech pánve pro každou
+            rekonstrukce. Nemů
+          </p>
+          <Link to="/">Domů</Link>
+          <Link to="/about">About</Link>
+        </div>
       </div>
     );
   }
